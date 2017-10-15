4b052dd666b00a5ab8428e91d462004ed9abc3d1
minciong <adhdtv@bitbucket.org>
2017-10-14 21:43:05 -0400

    yay made the get tags function work

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ from clarifai import *
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import client
 import json
-app = ClarifaiApp(api_key='f057c497d264409f8c50bcfa79666b94')
+app = ClarifaiApp(api_key='eabb3a40609c4b8096054351b85d68f5')
 #img = ClImage(filename='/tmp/user/dog.jpg')
 
-#def getTags(imgurl)
-# get the general model
-model = app.models.get("bd367be194cf45149e75f01d59f77ba7")
-# predict with the model
-food=model.predict_by_url(url="https://samples.clarifai.com/food.jpg")#imgurl
-print(str(food))
-parsed=json.loads(str(food).replace("'", '"'))
-for each in parsed:
-    print(each)
+def getTags(imgurl)
+	# get the general model
+	model = app.models.get("bd367be194cf45149e75f01d59f77ba7")
+	# predict with the model
+	food=model.predict_by_url(url=imgurl)#"https://samples.clarifai.com/food.jpg")#imgurl
+	tags=[]
+	for each in food['outputs'][0]['data']['concepts']:
+		if each['value']>0.95:
+			tags.append(each['name'])
+	if len(tags)==0:
+		print("No Tags")
+		sys.exit(-1)
+	#for each in tags:
+	#print(each)
+	return tags
+
 
