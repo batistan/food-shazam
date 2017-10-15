6c4fd259034c39fc40629a34005f8e46de545279
minciong <adhdtv@bitbucket.org>
2017-10-14 20:19:11 -0400

    test clarifai

---
+++ test.py
@@ -0,0 +1,16 @@
+from clarifai import *
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import client
+import json
+app = ClarifaiApp(api_key='f057c497d264409f8c50bcfa79666b94')
+#img = ClImage(filename='/tmp/user/dog.jpg')
+
+#def getTags(imgurl)
+# get the general model
+model = app.models.get("bd367be194cf45149e75f01d59f77ba7")
+# predict with the model
+food=model.predict_by_url(url="https://samples.clarifai.com/food.jpg")#imgurl
+parsed=json.loads(str(food))
+print(parsed['food'])
+	
+
